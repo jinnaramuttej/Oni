8c699d8c158a589ba04ae0dd5f68ee44ef5d6dd0
Uttej <jinnaramuttej@gmail.com>
2026-05-30 18:11:22 +0530

    landing page

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { HeroGeometric } from "@/components/ui/shape-landing-hero";
+
+export default function Home() {
+  return (
+    <HeroGeometric
+      badge="Oni Platform"
+      title1="Agentic AI Software"
+      title2="Development Platform"
+    />
+  );
+}
